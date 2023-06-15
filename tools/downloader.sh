c3b2ace33dd5d50f9e2018f0c53f8d55247dceb0
Ciprian Mandache <ciprian@51k.eu>
2023-06-15 09:25:37 +0300

    check if jq and wget are installed before downloading the latest version

--- tools/downloader.sh
+++ tools/downloader.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-owner=psyb0t
-repo=ssh-tunnel-swarm
-asset_name=ssh-tunnel-swarm
+
+# Check if jq is installed
+echo "Checking if jq is installed..."
+if ! command -v jq &>/dev/null; then
+    echo "jq is required but not installed. Please install jq (e.g., 'sudo apt-get install jq') and try again."
+    exit 1
+fi
+echo "jq is installed."
+
+# Check if wget is installed
+echo "Checking if wget is installed..."
+if ! command -v wget &>/dev/null; then
+    echo "wget is required but not installed. Please install wget (e.g., 'sudo apt-get install wget') and try again."
+    exit 1
+fi
+echo "wget is installed."
+
+owner="psyb0t"
+repo="ssh-tunnel-swarm"
+asset_name="ssh-tunnel-swarm"
 
 echo "Looking up the latest release of $asset_name for github.com/$owner/$repo..."
 releases=$(wget -qO- "https://api.github.com/repos/$owner/$repo/releases")
